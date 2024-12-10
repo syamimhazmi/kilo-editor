45e6b9c0b876c89365a1a6d206aa011ff6c73f29
syamimhazmi <syamimhazmi@gmail.com>
2024-12-11 03:34:06 +0700

    disabling ctrl-s and ctrl-q

--- kilo.c
+++ kilo.c
@@ -7,64 +7,75 @@
 struct termios origin_termios;
 
 void disableRawMode(void) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin_termios);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin_termios);
 }
 
 void enableRawMode(void) {
-    /* tcgetattr() is used to read current attribute into a struct */
-    tcgetattr(STDIN_FILENO, &origin_termios);
+  /* tcgetattr() is used to read current attribute into a struct */
+  tcgetattr(STDIN_FILENO, &origin_termios);
 
-    /* 
-     * atexit method is used when the program exits 
-     * either by returning to main() or by calling exit()
-     */
-    atexit(disableRawMode);
+  /*
+   * atexit method is used when the program exits
+   * either by returning to main() or by calling exit()
+   */
+  atexit(disableRawMode);
 
-    struct termios raw = origin_termios;
+  struct termios raw = origin_termios;
 
-    /* 
-     * ECHO features enable program to print each key that are pressed
-     * the code we write to disable ECHO
-     * the tilde (~) is a bitwise NOT OPERATOR
-     * using ICON flag allow us to turn off canonical mode 
-     * using ISIG flag allow us to turn off `ctrl-c` and `ctrl-z` signal
-     * `ctrl-c` sends a SIGNINT signal to the current process which causes it to terminate,
-     * `ctrl-z` sends a SIGSTP signal to the current process which causes it to suspend.
-    */
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  /**
+   * By default, Ctrl-S and Ctrl-Q are used for software flow control.
+   * Ctrl-S stops data from being transmitted to the terminal until you press
+   * Ctrl-Q. IXON comes from <termios.h>. The I stands for “input flag” (which
+   * it is, unlike the other I flags we’ve seen so far) and XON comes from the
+   * names of the two control characters that Ctrl-S and Ctrl-Q produce: XOFF to
+   * pause transmission and XON to resume transmission.
+   */
+  raw.c_iflag &= ~(IXON);
+  /*
+   * ECHO features enable program to print each key that are pressed
+   * the code we write to disable ECHO
+   * the tilde (~) is a bitwise NOT OPERATOR
+   * using ICON flag allow us to turn off canonical mode
+   * using ISIG flag allow us to turn off `ctrl-c` and `ctrl-z` signal
+   * `ctrl-c` sends a SIGNINT signal to the current process which causes it to
+   * terminate, `ctrl-z` sends a SIGSTP signal to the current process which
+   * causes it to suspend.
+   */
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
-    /*
-     * tcsetattr() method is a POSIX system call that sets terminal attributes/parameters
-     * the first param is file descripter(filde) for the terminal(like STDIN_FILENO).
-     * the second param is when to apply changes. since we are suing TCSAFLUSH, it will
-     * discard any input that hasn't been read.
-     * the third param is the pointer to the termios struc containing new attribute.
-     * this method will return an integer (0 on success and -1 on error).
-    */
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  /*
+   * tcsetattr() method is a POSIX system call that sets terminal
+   * attributes/parameters the first param is file descripter(filde) for the
+   * terminal(like STDIN_FILENO). the second param is when to apply changes.
+   * since we are suing TCSAFLUSH, it will discard any input that hasn't been
+   * read. the third param is the pointer to the termios struc containing new
+   * attribute. this method will return an integer (0 on success and -1 on
+   * error).
+   */
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(void) {
-    enableRawMode();
+  enableRawMode();
 
-    char c;
-    /* read method enable use to read one byte from standard input
-     * into a varibale c
-     * here we add when user press q, it will exit the program
-    */
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        /* 
-         * iscntrl() method comes from <ctype.h>
-         * it uses is to tests whether a character is a control character based on ASCII
-         * control characters are nonprintable character that we don't want to print to the screen.
-         * ASCII code 0-31 are all control characters, and 127 is also a control character.
-         * ASCII code 32-126 are all printable.
-         */
-        if (iscntrl(c)) {
-            printf("%d\n", c);
-        } else {
-            printf("%d ('%c')\n", c, c);
-        }
+  char c;
+  /* read method enable use to read one byte from standard input
+   * into a varibale c
+   * here we add when user press q, it will exit the program
+   */
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    /*
+     * iscntrl() method comes from <ctype.h>
+     * it uses is to tests whether a character is a control character based on
+     * ASCII control characters are nonprintable character that we don't want to
+     * print to the screen. ASCII code 0-31 are all control characters, and 127
+     * is also a control character. ASCII code 32-126 are all printable.
+     */
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
     }
-    return 0;
+  }
+  return 0;
 }
